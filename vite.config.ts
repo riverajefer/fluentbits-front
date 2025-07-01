5506301e467c0f3450d00b6253a53ffcbf825ce2
Jefferson Rivera <jefersonpatino@yahoo.es>
2025-07-01 12:14:58 -0500

    chore: configure environment variables and deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configuración para despliegue en GitHub Pages
-  base: '',
-  // base: '/fluentbits-front/',
-  
+  base: '/FluentBits-front/',
 })
+
